14252e6cb9e64d158eea7da260b6cd759b4cc311
noahthomlison <noahthomlison@gmail.com>
2022-03-16 06:51:51 -0700

    adds in project data from data file

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: "JamSpace",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus porta massa, in dapibus nisl hendrerit eget. Proin nulla risus, pharetra et convallis eget, tincidunt non magna. ", 
+    techStack: ["JavaScript", "Mongo"],
+    githubLink: "https://github.com/NoahThomlison/JamSpace",
+    images: ["https://github.com/bbjarvis/JamSpace/blob/master/screenshots/Map_Listing.gif"],
+  },
+  {
+    title: "JamSpace",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus porta massa, in dapibus nisl hendrerit eget. Proin nulla risus, pharetra et convallis eget, tincidunt non magna. ", 
+    techStack: ["JavaScript", "Mongo"],
+    githubLink: "https://github.com/NoahThomlison/JamSpace",
+    images: ["https://github.com/bbjarvis/JamSpace/blob/master/screenshots/Map_Listing.gif"],
+  },
+  {
+    title: "JamSpace",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus porta massa, in dapibus nisl hendrerit eget. Proin nulla risus, pharetra et convallis eget, tincidunt non magna. ", 
+    techStack: ["JavaScript", "Mongo"],
+    githubLink: "https://github.com/NoahThomlison/JamSpace",
+    images: ["https://github.com/bbjarvis/JamSpace/blob/master/screenshots/Map_Listing.gif"],
+  },
+]
