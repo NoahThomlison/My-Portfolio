b3d54865a1358f3bcee7cb59b0b98693061fe0ee
noahthomlison <noahthomlison@gmail.com>
2022-03-16 06:52:44 -0700

    adds in mapping over static data to start to build layout

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,8 +1,19 @@
 import {Container, Box, Paper} from '@mui/material';
 
-function Work() {
+function Work(props) {
+  const {projects} = props
+  console.log(projects)
   return (
     <section>
+      {projects.map((project) => {
+        return(
+          <div>
+            <h1>{project.title}</h1>
+            <p>{project.description}</p>
+            <p>{project.githubLink}</p>
+          </div>
+        )
+      })}
     </section>
   )
 }
