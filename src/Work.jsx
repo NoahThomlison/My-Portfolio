28f33c5a99793f9d4c0874f35e679139faa96b61
noahthomlison <noahthomlison@gmail.com>
2022-03-18 13:44:06 -0700

    adds layout of implimentation for MUI image cards

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,20 +1,35 @@
-import {Container, Box, Paper} from '@mui/material';
+import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper} from '@mui/material';
 
 function Work(props) {
   const {projects} = props
   console.log(projects)
   return (
-    <section>
+    <Container sx={{display: "Flex"}}>
       {projects.map((project) => {
         return(
-          <div>
-            <h1>{project.title}</h1>
-            <p>{project.description}</p>
-            <p>{project.githubLink}</p>
-          </div>
+          <Card sx={{ maxWidth: 345, margin: "10px" }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              {project.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              {project.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         )
       })}
-    </section>
+    </Container>
   )
 }
 
