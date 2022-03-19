9daa6180c53e2241655bc7b763e0aee1d785cc01
noahthomlison <noahthomlison@gmail.com>
2022-03-19 10:42:01 -0700

    initial commit of filter.jsx which will be used to filter projects based on stack

---
+++ src/Filter.jsx
@@ -0,0 +1,14 @@
+import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper, IconButton, SvgIcon, FontAwesomeSvgIcon, Icon } from '@mui/material';
+
+
+function Filter(props) {
+
+  const {projects} = props
+  console.log(projects)
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default Filter;
