ad126a23d43130bdc7963ea368f65f8992a0c05e
noahthomlison <noahthomlison@gmail.com>
2022-03-20 18:52:46 -0700

    adds buttons for the tech stack

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,13 +1,21 @@
-import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper, IconButton, SvgIcon, FontAwesomeSvgIcon, Icon } from '@mui/material';
+import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper, IconButton, SvgIcon, FontAwesomeSvgIcon, Icon, ButtonGroup } from '@mui/material';
 
 
 function Filter(props) {
 
   const {projects} = props
-  console.log(projects)
+  let stackList = ["React", "JS", "Python", "Ruby", "Rails", "Mongo", "Express", "Node.js", "Bootstrap", "MUI", "APIs", "SASS"];
+
   return (
-    <Container>
-    </Container>
+    <Box sx={{display: "flex", justifyContent: "center"}}>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        {stackList.map((stack)=> {
+          return(
+            <Button>{stack}</Button>
+          )
+        })}
+      </ButtonGroup>
+    </Box>
   )
 }
 
