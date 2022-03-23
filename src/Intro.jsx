6321ba413df94240008ad5e0b2a61f5a4213efc4
noahthomlison <noahthomlison@gmail.com>
2022-03-23 05:48:41 -0700

    removes typing animation

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,26 +1,20 @@
 import TypeAnimation from 'react-type-animation';
-import {Container, Box, Paper} from '@mui/material';
+import {Container, Box, Paper, Typography} from '@mui/material';
 
 const Intro = () => {
   return (
     <Container>
-      <Paper>
-        <TypeAnimation cursor={false} sequence={["", 2000, 'Hi, Im Noah!']}wrapper="h1"/>
-        <TypeAnimation
-        cursor={false}
-        sequence={['', 
-        3000, 'Im a mechanical engineer.', 
-        1000, 'Im a full stack web developer.',
-        1000, 'Im a full stack web developer and I like to pet dogs.',
-        1000, 'Im a full stack web developer and I like to ride motorcycles.',
-        1000, 'Im a full stack web developer and I like to code.',
-        1000, 'Im a full stack web developer and I like to design.',
-        1000, 'Im a full stack web developer and I like to learn.',
-        1000, 'Im a full stack web developer and I like to make things.',
-        ]}
-        wrapper="h1"
-        />
-      </Paper>
+      {/* <Paper sx={{padding: "20px", top: "0px"}}> */}
+        <Typography variant="h1">
+          Hi, Im Noah.
+        </Typography>
+        <Typography variant="h2">
+          Full Stack Web Developer. 
+        </Typography>
+        <Typography variant="h4">
+          JS, React, Python, Ruby.        
+        </Typography>
+      {/* </Paper> */}
     </Container>
   );
 };
