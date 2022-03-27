242b990d99f85ba47adbe072315b29b03ed701b0
noahthomlison <noahthomlison@gmail.com>
2022-03-26 23:31:47 -0700

    adds in conditional rendering when there are  no projects for chosen filter

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -4,21 +4,26 @@ import Project from "./Project"
 import React, { useState } from 'react';
 
 function ProjectList({projects}) {
-  console.log(projects)
   const [filter, setFilter] = useState("All");
-
   const projectsToRender = projects.filter(function (project) {
     return((project.techStack.includes(filter) || filter === "All"))
   })
 
   return (
-    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "75px"}}>
-      <Filter sx={{justifySelf: "flex-start"}}projects={projects} setFilter={setFilter}></Filter>
+    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "10px"}}>
+      <Filter sx={{justifySelf: "flex-start"}} projects={projects} setFilter={setFilter}></Filter>
       <Box sx={{display: "Flex", flexWrap: "wrap", padding: "100px, 10px, 0px, 10px", justifyContent: "center", height: "90%"}}>
-      {projectsToRender.map((project) => {
-        return(<Project project={project}></Project>)
-      })}
-      </Box>
+          {projectsToRender.length === 0 &&
+            <Box sx={{paddingTop: "50px"}}>
+              <Typography variant="h5">Nothing Yet.</Typography>
+            </Box>}
+          {projectsToRender.length > 0 &&
+            projectsToRender.map((project, index) => {
+              return(<Project project={project} index={index}></Project>)
+            })
+          }
+    </Box>
+
     </Box>
   )
 }
