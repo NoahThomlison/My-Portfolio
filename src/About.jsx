13a7ff4046f88bae65eb28b674b789511e70fcae
noahthomlison <noahthomlison@gmail.com>
2022-03-27 10:59:57 -0700

    updates contents for the about me bio

--- src/About.jsx
+++ src/About.jsx
@@ -1,24 +1,30 @@
-import {Container, Box, Paper} from '@mui/material';
+import {Container, Box, Paper, Typography} from '@mui/material';
 
 function About() {
   return (
-    <Container>
-      <Paper sx={{padding: "10px", display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <Box sx={{padding: "10px"}}>
-          <img src={require('./images/profilePic.jpg')} className="profilePicture"/>
-        </Box>
-        <Box  sx={{padding: "10px"}}>
-          <h2>About me</h2>
-          <h3>Thrid Person Bio</h3>
-          {/* <p>Noah is a full-stack web developer. He attended a 6 month flex web development bootcamp in 2022. Prior to this he was a mechanical engineer with a degree in   mechanical engineering. He has experiance with</p> */}
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a risus quis magna porta posuere quis eget justo. Nulla a ex eget sapien auctor sodales. Mauris   egestas vulputate neque at sodales. Pellentesque ut ipsum id nibh fermentum lacinia vitae eget magna. Nunc nec nisl ante. Aenean dapibus neque quis porta iaculis.  Vivamus mattis, est et tempor posuere, erat mi cursus ligula, quis lobortis dui nisl id urna. Nulla dolor dolor, commodo et arcu sed, commodo consectetur magna. Etiam   ut neque in ex tempor pulvinar a eget leo. Proin consequat arcu eu est gravida fermentum. Suspendisse convallis augue euismod quam vehicula, eget consectetur turpis  congue. Curabitur sed nisi mi. Sed pretium est ante, quis varius nibh condimentum quis. Pellentesque ultricies nunc tortor, a cursus metus aliquam ut.
-
-          Nunc tempor luctus dui nec luctus. Curabitur eleifend vestibulum consequat. Sed sagittis mi metus, at lacinia orci luctus sagittis. Proin sollicitudin est sit amet elit  sodales pulvinar. Praesent sodales nunc vitae nulla semper porta volutpat a justo. Sed sem tellus, dignissim id mi at, feugiat cursus augue. Vestibulum iaculis ex sed   porta posuere.
-
-          Fusce molestie ipsum vitae lectus lobortis, vel condimentum sem porta. Aliquam eget quam sed nisi consectetur molestie. Proin commodo dui mi.</p>
-        </Box>
-      </Paper>
-    </Container>
+    <Box sx={{padding: "10px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <Box sx={{padding: "10px"}}>
+        <img src={require('./images/profilePic.jpg')} className="profilePicture"/>
+      </Box>
+      <Box  sx={{padding: "10px"}}>
+        <Typography variant="h4">About me</Typography>
+        <Typography>
+            I am a recent graduate of a six-month-long boot camp at Lighthouse Labs, where I obtained a diploma in Web Development. I also have a Bachelor's Degree in Mechanical Engineering from the British Columbia Institue of Technology.
+        </Typography>
+        <br></br>
+        <Typography>
+            At Lighthouse Labs, I gained experience with various technologies and frameworks including NodeJS, APIs and React. Lighthouse Labs has also taught me testing   frameworks and libraries such as Cypress, Storybook and Jest.
+        </Typography>
+        <br></br>
+        <Typography>
+            Before attending Lighthouse Labs I have worked as a Mechanical Design Engineer for 5 yearsand have worked in various industries designing consumer goods. This  includes sporting goods, motorcycle accessories and commercial cameras. I decided to make a pivot from Mechanical Engineering to Web Development as I have found the speed and ability to rapidly iterate and design when doing coding is something I greatly enjoy.
+        </Typography>          
+        <br></br>
+        <Typography>
+            In my free time, I am continuing my studies by learning Python and Django, practicing to hone my skills further in React and refactoring my midterm project,  Smart-To-Do list, to add new functionality.
+        </Typography>
+      </Box>
+    </Box>
   )
 }
 
