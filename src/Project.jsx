a4aadb4b5b61e4c7353fcd84240b4de128179757
noahthomlison <noahthomlison@gmail.com>
2022-03-26 23:32:10 -0700

    adds links and styling to the view on github button

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,39 +1,35 @@
-import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper} from '@mui/material';
+import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper, Link} from '@mui/material';
 
-function Project(props) {
-  const {project} = props
-
-
+function Project({project, index}) {
+  
   return (
-            <Card sx={{ width: 400, minWidth: 300, margin: "10px", height: "300px" }}>
-              <CardMedia
-                component="img"
-                height="140"
-                image={project.image}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                {project.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                {project.description}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                <br></br>
-                <strong>Stack:</strong> {project.techStack.map((stack, index) => {
-                  return(stack + " ")})}
-                </Typography>
-              </CardContent>
-              <Box sx={{display:"flex", justifyContent: "center", bottom: "0px", position:"relative"}}>
-              <CardActions>
-                <Button size="small">More Info</Button>
-              </CardActions>
-              <CardActions>
-                <Button size="small">View on Github</Button>
-              </CardActions>
-              </Box>
-            </Card>
+    <Card sx={{ width: 350, minWidth: 300, margin: "10px", height: "300px", position: "relative"}}>
+      <CardMedia
+        component="img"
+        height="50%"
+        image={project.image}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h7" component="div">
+        {project.title}
+        </Typography>
+        {/* <Typography variant="body2" color="text.secondary">
+        {project.description}
+        </Typography> */}
+        <Typography variant="body2" color="text.secondary">
+        <strong>Stack:</strong> {project.techStack.map((stack, index) => {
+          return(stack + " ")})}
+        </Typography>
+      </CardContent>
+      <Box sx={{position: "absolute", bottom: "0px"}}>
+      <CardActions>
+        <Link href={project.githubLink} underline="none">
+        <Button size="small" variant="outlined">View on Github</Button>
+        </Link>
+      </CardActions>
+      </Box>
+    </Card>
   )
 }
 
