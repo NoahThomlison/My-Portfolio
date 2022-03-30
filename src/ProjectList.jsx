c1761b9b15fa061fd542c2f8d23aec3a5cbb5c5a
noahthomlison <noahthomlison@gmail.com>
2022-03-30 12:06:08 -0700

    adds themeprovide, moves filters out of projectList, increases top padding

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -1,30 +1,31 @@
-import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper} from '@mui/material';
+import {Container, Typography, CardMedia, Card, CardActions, CardContent, Button, Box, Paper, ThemeProvider} from '@mui/material';
 import Filter from "./Filter"
 import Project from "./Project"
 import React, { useState } from 'react';
 
-function ProjectList({projects}) {
+function ProjectList({projects, theme}) {
   const [filter, setFilter] = useState("All");
   const projectsToRender = projects.filter(function (project) {
     return((project.techStack.includes(filter) || filter === "All"))
   })
 
   return (
-    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "10px"}}>
-      <Filter sx={{justifySelf: "flex-start"}} projects={projects} setFilter={setFilter}></Filter>
-      <Box sx={{display: "Flex", flexWrap: "wrap", padding: "100px, 10px, 0px, 10px", justifyContent: "center", height: "90%"}}>
-          {projectsToRender.length === 0 &&
-            <Box sx={{paddingTop: "50px"}}>
-              <Typography variant="h5">Nothing Yet.</Typography>
-            </Box>}
-          {projectsToRender.length > 0 &&
-            projectsToRender.map((project, index) => {
-              return(<Project project={project} index={index} image={project.image}></Project>)
-            })
-          }
-    </Box>
-
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        {/* <Filter sx={{position: "fixed"}} projects={projects} setFilter={setFilter}></Filter> */}
+        <Box sx={{ display: "Flex", flexWrap: "wrap", padding: "100px, 80px, 0px, 10px", paddingTop: "70px", justifyContent: "center", height: "90%"}}>
+            {projectsToRender.length === 0 &&
+              <Box sx={{paddingTop: "50px"}}>
+                <Typography variant="h5">Nothing Yet.</Typography>
+              </Box>}
+            {projectsToRender.length > 0 &&
+              projectsToRender.map((project, index) => {
+                return(<Project project={project} index={index} image={project.image}></Project>)
+              })
+            }
+        </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
 
