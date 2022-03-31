74b84fb3927fa563c9424e90115a2ddaf63d66d9
noahthomlison <noahthomlison@gmail.com>
2022-03-31 07:24:34 -0700

    moves fullpage section wrapped to intro section

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -4,24 +4,27 @@ import {Container, Box, Paper, Typography, Button, ButtonGroup, ThemeProvider } 
 
 const Intro = ({fullpageApi, theme}) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container sx={{position: "relative", bottom: "150px", color:"#FFFFFF", paddingTop: "100px"}}>
-          <Typography variant="h1">
-            Hi, Im Noah.
-          </Typography>
-          <Typography variant="h2">
-            Full Stack Web Developer. 
-          </Typography>
-          <Typography variant="h4">
-            JS, React, Python, Ruby.        
-          </Typography>
-          <Box sx={{display:"flex", marginTop: "15px"}}>
-            <Button variant="outlined" onClick={() => fullpageApi.moveTo(2)} sx={{marginRight: "20px", borderColor:"#FFFFFF"}}>About me</Button>
-            <Button variant="outlined" onClick={() => fullpageApi.moveTo(3)} sx={{borderColor:"#FFFFFF"}}>My Work</Button>
-            {/* <Button variant="outlined" onClick={() => fullpageApi.moveTo(4)} sx={{marginLeft: "20px", borderColor:"#FFFFFF"}}>Contact me</Button> */}
-          </Box>
-      </Container>
+    <div className="section heroImageAlt">
+      <ThemeProvider theme={theme}>
+
+        <Container sx={{position: "relative", bottom: "150px", color:"#FFFFFF", paddingTop: "100px"}}>
+            <Typography variant="h1">
+              Hi, Im Noah.
+            </Typography>
+            <Typography variant="h2">
+              Full Stack Web Developer. 
+            </Typography>
+            <Typography variant="h4">
+              JS, React, Python, Ruby.        
+            </Typography>
+            <Box sx={{display:"flex", marginTop: "15px"}}>
+              <Button variant="outlined" onClick={() => fullpageApi.moveTo(2)}  sx={{marginRight: "20px", borderColor:"#FFFFFF"}}>About me</Button>
+              <Button variant="outlined" onClick={() => fullpageApi.moveTo(3)}  sx={{borderColor:"#FFFFFF"}}>My Work</Button>
+              {/* <Button variant="outlined" onClick={() => fullpageApi.moveTo(4) } sx={{marginLeft: "20px", borderColor:"#FFFFFF"}}>Contact me</  Button> */}
+            </Box>
+        </Container>
     </ThemeProvider>
+    </div>
 
   );
 };
