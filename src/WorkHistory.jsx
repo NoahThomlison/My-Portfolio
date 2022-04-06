cb3a84784be331c8b15cc0e5670d76bc041e8979
noahthomlison <noahthomlison@gmail.com>
2022-04-05 21:19:34 -0700

    adds mui elements, creates basic layout

--- src/WorkHistory.jsx
+++ src/WorkHistory.jsx
@@ -4,71 +4,52 @@ function Education({theme}) {
   return (
     <ThemeProvider theme={theme}>
       <Container>
-        <table>
-          <tbody>
-            <tr>
-              <td>Motorola Solutions</td>
-              <td>Mechanical Design Engineer</td>
-              <td>Vancouver, British Columbia</td>
-              <td>May 2021 to Current</td>
-            </tr>
-            <tr>
-              <td colspan="3">
-                <p>Work in a multi-disciplined team to develop commercial high-end security cameras</p>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="3">
-              <p>Coordinate with international suppliers to source and plan project timelines</p>
-            </td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-            <tr>
-              <td>WestMill Industries</td>
-              <td>Mechanical Design Engineer</td>
-              <td>Abbotsford, British Columbia</td>
-              <td>May 2019​ to May 2021​</td>
-            </tr>
-            <tr>
-              <td colspan="3">
-                <p>Design large scale-forestry equipment and machinery</p>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="3">
-              <p>Creation of project plans with project managers</p>
-            </td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-            <tr>
-              <td>Modular Driven Technologies</td>
-              <td>Design Engineer</td>
-              <td>Chilliwack, British Columbia</td>
-              <td>May 2016​ to May 2019​</td>
-            </tr>
-            <tr>
-              <td colspan="3">
-                <p>Design, sourcing and testing of consumer sporting goods</p>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="3">
-              <p>Manage projects for new products, lead design meetings and product kickoff meetings</p>
-            </td>
-            </tr>
-          </tbody>
-        </table>
+        <Box className="underline">
+          <Typography>Motorola Solutions</Typography>
+        </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+          <Box>
+            <Typography>Mechanical Design Engineer</Typography>
+            <Typography>Vancouver, British Columbia</Typography>
+          </Box>
+          <Typography>May 2021 to Current</Typography>
+        </Box>
+        <ul>
+          <li><Typography>Work in a multi-disciplined team to develop commercial high-end security cameras</Typography></li>
+          <li><Typography>Coordinate with international suppliers to source and plan project timelines</Typography></li>
+        </ul>
+        <Box className="underline">
+          <Typography>WestMill Industries</Typography>
+        </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+          <Box>
+            <Typography>Mechanical Design Engineer</Typography>
+            <Typography>Abbotsford, British Columbia</Typography>
+          </Box>
+          <Typography>May 2019​ to May 2021​</Typography>
+        </Box>
+        <ul>
+          <li><Typography>Design large scale-forestry equipment and machinery</Typography></li>
+          <li><Typography>Creation of project plans with project managers</Typography></li>
+        </ul>
+        <Box className="underline">
+          <Typography>Modular Driven Technologies</Typography>
+        </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+          <Box sx={{display: "flex"}}>
+            <Typography>Design Engineer</Typography>
+            <Typography>Chilliwack, British Columbia</Typography>
+          </Box>
+          <Typography>May 2016​ to May 2019​</Typography>
+        </Box>
+        <ul>
+          <li><Typography>Design, sourcing and testing of consumer sporting goods</Typography></li>
+          <li><Typography>Manage projects for new products, lead design meetings and product kickoff meetings</Typography></li>
+        </ul>
       </Container>
     </ThemeProvider>
   )
 }
-
 
 export default Education;
 
