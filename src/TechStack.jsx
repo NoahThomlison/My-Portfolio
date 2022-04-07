78e6c8c8bdf0a27c73daa76ce4379de5349deed5
noahthomlison <noahthomlison@gmail.com>
2022-04-06 20:03:39 -0700

    Restructures content and modifys styling

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -3,47 +3,41 @@ import {Container, Box, Paper, Typography, ThemeProvider} from '@mui/material';
 function TechStack({theme}) {
   return (
     <ThemeProvider theme={theme}>
-      <Box className="flex-container" sx={{display: "flex", flexDirection: "column"}}>
-      <Box sx={{flex: "1"}}>
-        <Box className="underline" >
-          <Typography variant="h5" sx={{paddingLeft: "5px"}}>Languages</Typography>
-        </Box>
-        <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
-        <Typography>JavaScript, CSS, HTML, Ruby, C, Python</Typography>
-        </Box>
-        </Box>
-
-
-        <Box sx={{flex: "1"}}>
-
-        <Box className="underline" >
-          <Typography variant="h5" sx={{paddingLeft: "5px"}}>Frameworks, Libraries, Enviroments</Typography>
-        </Box>
-                <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
-                <Typography>NodeJS, React, Ajax, Express, EJS, Jquery, ActiveRecord, SASS, Ruby on Rails, Bootstrap, Material UI</Typography>
-        </Box>
-        </Box>
-        <Box sx={{flex: "1"}}>
-
-        <Box className="underline" >
-          <Typography variant="h5" sx={{paddingLeft: "5px"}}>Testing</Typography>
-        </Box>         
-        <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
-        <Typography>RSpec, Cypress, Storybook, Jest, Mocha, Chai</Typography>
-        </Box>
-                </Box>
-
-                <Box sx={{flex: "1"}}>
-
-        <Box className="underline" >
-          <Typography variant="h5" sx={{paddingLeft: "5px"}}>Databases</Typography>
-        </Box>
-        <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
-        <Typography>PostgreSQL, SQL, MongoDB</Typography>
-        </Box>
+        <Box>
+          <Box className="underline" >
+            <Typography variant="h6" sx={{paddingLeft: "5px"}}>Languages</Typography>
+          </Box>
+          <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
+            <Typography>JavaScript, CSS, HTML, Ruby, C, Python</Typography>
+          </Box>
         </Box>
 
-      </Box>
+        <Box>
+          <Box className="underline" >
+            <Typography variant="h6" sx={{paddingLeft: "5px"}}>Frameworks, Libraries, Enviroments</Typography>
+          </Box>
+          <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
+            <Typography>NodeJS, React, Ajax, Express, EJS, Jquery, ActiveRecord, SASS, Ruby on Rails, Bootstrap, Material UI</Typography>
+          </Box>
+        </Box>
+        
+        <Box>
+          <Box className="underline" >
+            <Typography variant="h6" sx={{paddingLeft: "5px"}}>Testing</Typography>
+          </Box>         
+          <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
+            <Typography>RSpec, Cypress, Storybook, Jest, Mocha, Chai</Typography>
+          </Box>
+        </Box>
+
+        <Box>
+          <Box className="underline" >
+            <Typography variant="h6" sx={{paddingLeft: "5px"}}>Databases</Typography>
+          </Box>
+          <Box sx={{paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
+            <Typography>PostgreSQL, SQL, MongoDB</Typography>
+          </Box>
+        </Box>
     </ThemeProvider>
   )
 }
--- src/WorkHistory.jsx
+++ src/WorkHistory.jsx
@@ -3,11 +3,11 @@ import {Container, Box, Paper, Typography, ThemeProvider} from '@mui/material';
 function Education({theme}) {
   return (
     <ThemeProvider theme={theme}>
-      <Container>
+      <Box>
         <Box className="underline">
-          <Typography>Motorola Solutions</Typography>
+          <Typography variant="h6">Motorola Solutions</Typography>
         </Box>
-        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+        <Box sx={{display: "flex", justifyContent: "space-between", paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
           <Box>
             <Typography>Mechanical Design Engineer</Typography>
             <Typography>Vancouver, British Columbia</Typography>
@@ -18,25 +18,33 @@ function Education({theme}) {
           <li><Typography>Work in a multi-disciplined team to develop commercial high-end security cameras</Typography></li>
           <li><Typography>Coordinate with international suppliers to source and plan project timelines</Typography></li>
         </ul>
+      </Box>
+
+      <Box>
         <Box className="underline">
-          <Typography>WestMill Industries</Typography>
+          <Typography variant="h6">WestMill Industries</Typography>
         </Box>
-        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+        <Box sx={{display: "flex", justifyContent: "space-between", paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
           <Box>
             <Typography>Mechanical Design Engineer</Typography>
             <Typography>Abbotsford, British Columbia</Typography>
           </Box>
           <Typography>May 2019​ to May 2021​</Typography>
         </Box>
+        <Box sx={{display: "flex", justifyContent: "space-between", paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
         <ul>
           <li><Typography>Design large scale-forestry equipment and machinery</Typography></li>
           <li><Typography>Creation of project plans with project managers</Typography></li>
         </ul>
+        </Box> 
+      </Box>
+
+      <Box>
         <Box className="underline">
-          <Typography>Modular Driven Technologies</Typography>
+          <Typography variant="h6">Modular Driven Technologies</Typography>
         </Box>
-        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-          <Box sx={{display: "flex"}}>
+        <Box sx={{display: "flex", justifyContent: "space-between", paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px"}}>
+          <Box>
             <Typography>Design Engineer</Typography>
             <Typography>Chilliwack, British Columbia</Typography>
           </Box>
@@ -46,7 +54,7 @@ function Education({theme}) {
           <li><Typography>Design, sourcing and testing of consumer sporting goods</Typography></li>
           <li><Typography>Manage projects for new products, lead design meetings and product kickoff meetings</Typography></li>
         </ul>
-      </Container>
+      </Box>
     </ThemeProvider>
   )
 }
