f07c2cc8d87b11de3f2a3b5837158220e590ce60
noahthomlison <noahthomlison@gmail.com>
2022-04-06 21:38:11 -0700

    updates styling again to be responsive. Starts code to have classbased on clicks.

--- src/AboutList.jsx
+++ src/AboutList.jsx
@@ -10,23 +10,28 @@ import React, { useState } from 'react';
 function AboutList({theme}) {
   const [active, setActive] = useState("About")
 
+  function handleClick(activeSection) {
+    setActive(activeSection)
+
+  }
+
 
   return (
     <div className="section aboutMeImage">
     <ThemeProvider theme={theme}>
       <Container>
         <Paper sx={{display: "flex", justifyContent: "center", alignItems: "center", padding: ".75em"}}>
-          <Box sx={{padding: "1em", width: "40%"}} className="profilePictureBox">
+          <Box sx={{padding: "1em", width: "40%", maxHeight: "500px"}} className="profilePictureBox">
             <img src={require('./images/profilePic.jpg')} className="profilePicture"/>
           </Box>
           <Box className="profileText" sx={{position: "relative", display: "flex", flexDirection: "column", height: "100%", minHeight: "500px"}}>
-            <Box sx={{position: "absolute", top:"0px", display: "flex", width: "100%", justifyContent: "space-between"}}>
-              <Button sx={{width: "24%"}} variant="outlined" onClick={(()=> setActive("About"))}>Stuff about me</Button>
-              <Button sx={{width: "24%"}} variant="outlined" onClick={(()=> setActive("TechStack"))}>Tech I use</Button>
-              <Button sx={{width: "24%"}} variant="outlined" onClick={(()=> setActive("WorkHistory"))}>Where I worked</Button>
-              <Button sx={{width: "24%"}} variant="outlined" onClick={(()=> setActive("Education"))}>Where I learned</Button>
+            <Box sx={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+              <Button className={active === "About" ? 'test': null} sx={{width: "24%"}} variant="outlined" onClick={(()=> handleClick("About"))}>Stuff about me</Button>
+              <Button varient={active === "TechStack" ? 'outlined': null} sx={{width: "24%"}} variant="outlined" onClick={(()=> handleClick("TechStack"))}>Tech I use</Button>
+              <Button className={active ? 'test': null} sx={{width: "24%"}} variant="outlined" onClick={(()=> handleClick("WorkHistory"))}>Where I worked</Button>
+              <Button className={active ? 'test': null} sx={{width: "24%"}} variant="outlined" onClick={(()=> handleClick("Education"))}>Where I learned</Button>
             </Box>
-            <Box sx={{minHeight: "350px", display: "flex", flexDirection: "column", justifyContent: "flex-start", paddingTop: "50px"}}>
+            <Box sx={{minHeight: "350px", display: "flex", flexDirection: "column", justifyContent: "flex-start", paddingTop: "15px"}}>
               {active === "About" && <About theme={theme}></About>}
               {active === "TechStack" && <TechStack theme={theme}></TechStack>}
               {active === "WorkHistory" && <WorkHistory theme={theme}></WorkHistory>}
