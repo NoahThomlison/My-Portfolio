b8e9563c189b25ec6255d963d26e4946576373b6
noahthomlison <noahthomlison@gmail.com>
2022-04-11 16:41:04 -0700

    refactors heavily to get form submissions from template. Will refactor styling after form submissions corrected

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,25 +1,66 @@
 import {Container, Box, Paper, ThemeProvider, Button, Typography} from '@mui/material';
 import TextField from '@mui/material/TextField';
+import {useState, useEffect} from 'react'
 
 function Contact({theme}) {
-  const encode = (data) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+})
+
+const handleChange = e => {
+    const { name, value } = e.target
+    setFormData({
+        ...formData, 
+        [name]: value
+    })
+}
+
+const [errors, setErrors] = useState({})
+const validate = (formData) => {
+    let formErrors = {}
+    if(!formData.name){
+        formErrors.name = "Name required"
+    }
+    if(!formData.email){
+        formErrors.email = "Email required"
+    } 
+    if(!formData.message){
+        formErrors.message = "Message is required"
+    }
+    return formErrors
+}
+
+const [isSubmitted, setIsSubmitted] = useState(false)
+
+const handleSubmit = e => {
+    setErrors(validate(formData))
+    setIsSubmitted(true)
+    e.preventDefault();
+
+}
+
+const encode = (data) => {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
         .join("&");
   }
-  
-  const submitForm = (e) => {
-    e.preventDefault();
 
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
+useEffect(() => {
+    if(Object.keys(errors).length === 0 && isSubmitted){
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact-form", ...formData })
+        })
         .then(() => alert("Success!"))
-        .catch(error => alert(error));
-
-  };
+        .then(() => setIsSubmitted(false))
+        .then(() => setFormData({name: "", email: "",  message: ""}))
+        .catch(error => alert(error))
+    }
+}, [errors, formData, isSubmitted])
 
   return (
     <div className="section aboutMeImage">
@@ -29,12 +70,51 @@ function Contact({theme}) {
           <Box className="underline" sx={{width:"100%", display:"flex", justifyContent: "center"}} >
             <Typography variant="h6">Contact</Typography>
           </Box>
-            <form onSubmit={(e) => submitForm(e)} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-              <TextField sx={{minWidth: "90%"}} label="Name" type="Name"/> 
-              <TextField sx={{minWidth: "90%"}} label="Email" type="Email"/> 
-              <TextField sx={{minWidth: "90%"}} label="Message" name="Message" multiline rows={4}/>
-              <Button type="submit" sx={{minWidth: "25%"}} variant="outlined">Submit</Button> 
+          <div >
+            <h1> Sample Form </h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-inputs">
+                    <label htmlFor="name" className="form-label">
+                        Name
+                    </label>     
+                    <input 
+                        type="text" 
+                        name="name" 
+                        id="name" 
+                        className="form-input" 
+                        value={formData.name} 
+                        onChange={handleChange}/>
+                    {errors.name && <p>{errors.name}</p>}
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="email" className="form-label">
+                        Email
+                    </label>     
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        className="form-input" 
+                        value={formData.email} 
+                        onChange={handleChange}/>
+                    {errors.email && <p>{errors.email}</p>}
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="message" className="form-label">
+                        Message
+                    </label>     
+                    <textarea  
+                        name="message" 
+                        id="message" 
+                        className="form-input" 
+                        value={formData.message} onChange={handleChange} />
+                    {errors.message && <p>{errors.message}</p>}
+                </div>
+                <button type="submit" className="form-input-btn">
+                    Send
+                </button>
             </form>
+        </div>
           </Paper>
         </Container>
       </ThemeProvider>
@@ -43,3 +123,11 @@ function Contact({theme}) {
 }
 
 export default Contact;
+
+// {/* 
+//             <form onChange={(e) => handleSubmit(e)} name="contact" method="post"  data-netlify="true" data-netlify-honeypot="bot-field">
+//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "name")} value={name} label="name" type="name"/> 
+//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "email")} value={email} label="email" type="email"/> 
+//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "message")} value={message} label="message" name="message" multiline rows={4}/>
+//               <Button type="submit" sx={{minWidth: "25%"}} variant="outlined">Submit</Button> 
+//             </form> */} */}
