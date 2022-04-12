d4ad84a2446f5020a578768bb67695ead0618cd5
noahthomlison <noahthomlison@gmail.com>
2022-04-11 20:50:50 -0700

    redoes styling to use MUI components in form, adds classes for the form nad form inputs

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,114 +7,78 @@ function Contact({theme}) {
     name: "",
     email: "",
     message: ""
-})
+  })
 
-const handleChange = e => {
-    const { name, value } = e.target
-    setFormData({
-        ...formData, 
-        [name]: value
-    })
-}
-
-const [errors, setErrors] = useState({})
-const validate = (formData) => {
-    let formErrors = {}
-    if(!formData.name){
-        formErrors.name = "Name required"
-    }
-    if(!formData.email){
-        formErrors.email = "Email required"
-    } 
-    if(!formData.message){
-        formErrors.message = "Message is required"
-    }
-    return formErrors
-}
-
-const [isSubmitted, setIsSubmitted] = useState(false)
-
-const handleSubmit = e => {
-    setErrors(validate(formData))
-    setIsSubmitted(true)
-    e.preventDefault();
-
-}
-
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+  const handleChange = e => {
+      const { name, value } = e.target
+      setFormData({
+          ...formData, 
+          [name]: value
+      })
   }
 
-useEffect(() => {
-    if(Object.keys(errors).length === 0 && isSubmitted){
+  const [errors, setErrors] = useState({})
+  const validate = (formData) => {
+      let formErrors = {}
+      if(!formData.name){
+          formErrors.name = "Name required"
+      }
+      if(!formData.email){
+          formErrors.email = "Email required"
+      } 
+      if(!formData.message){
+          formErrors.message = "Message is required"
+      }
+      return formErrors
+  }
 
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact-form", ...formData })
-        })
-        .then(() => alert("Success!"))
-        .then(() => setIsSubmitted(false))
-        .then(() => setFormData({name: "", email: "",  message: ""}))
-        .catch(error => alert(error))
-    }
-}, [errors, formData, isSubmitted])
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const handleSubmit = e => {
+      setErrors(validate(formData))
+      setIsSubmitted(true)
+      e.preventDefault();
+  }
+
+  const encode = (data) => {
+      return Object.keys(data)
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+          .join("&");
+  }
+
+  useEffect(() => {
+      if(Object.keys(errors).length === 0 && isSubmitted){
+          fetch("/", {
+              method: "POST",
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+              body: encode({ "form-name": "contact-form", ...formData })
+          })
+          .then(() => console.log("Success!"))
+          .then(() => setIsSubmitted(false))
+          .then(() => setFormData({name: "", email: "",  message: ""}))
+          .catch(error => console.log(error))
+      }
+  }, [errors, formData, isSubmitted])
 
   return (
     <div className="section aboutMeImage">
       <ThemeProvider theme={theme}>
         <Container sx={{}}>
           <Paper sx={{display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center", padding: ".75em", minHeight: "500px"}}>
-          <Box className="underline" sx={{width:"100%", display:"flex", justifyContent: "center"}} >
-            <Typography variant="h6">Contact</Typography>
-          </Box>
-          <div >
-            <h1> Sample Form </h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-inputs">
-                    <label htmlFor="name" className="form-label">
-                        Name
-                    </label>     
-                    <input 
-                        type="text" 
-                        name="name" 
-                        id="name" 
-                        className="form-input" 
-                        value={formData.name} 
-                        onChange={handleChange}/>
-                    {errors.name && <p>{errors.name}</p>}
-                </div>
-                <div className="form-inputs">
-                    <label htmlFor="email" className="form-label">
-                        Email
-                    </label>     
-                    <input 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        className="form-input" 
-                        value={formData.email} 
-                        onChange={handleChange}/>
-                    {errors.email && <p>{errors.email}</p>}
-                </div>
-                <div className="form-inputs">
-                    <label htmlFor="message" className="form-label">
-                        Message
-                    </label>     
-                    <textarea  
-                        name="message" 
-                        id="message" 
-                        className="form-input" 
-                        value={formData.message} onChange={handleChange} />
-                    {errors.message && <p>{errors.message}</p>}
-                </div>
-                <button type="submit" className="form-input-btn">
-                    Send
-                </button>
+            <Box className="underline" sx={{width:"100%", display:"flex", justifyContent: "center"}} >
+              <Typography variant="h6">Contact</Typography>
+            </Box>
+            <form className="form" onSubmit={handleSubmit}>
+            <Box sx={{display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center", padding: ".75em", minHeight: "500px"}}>
+              <TextField type="text" name="name" id="name" className="form-input" label="Name" value={formData.name} onChange={handleChange}/>
+              {errors.name && <p>{errors.name}</p>}
+              <TextField type="email" name="email" id="email" className="form-input" label="Email" value={formData.email} onChange={handleChange}/>
+              {errors.email && <p>{errors.email}</p>}
+              <TextField name="message" id="message" className="form-input" label="Your Message" value={formData.message} multiline rows={4} onChange={handleChange} />
+              {errors.message && <p>{errors.message}</p>}
+              <Button type="submit" variant="outlined" className="form-input-btn">Submit</Button>
+              </Box>
             </form>
-        </div>
           </Paper>
         </Container>
       </ThemeProvider>
@@ -124,10 +88,8 @@ useEffect(() => {
 
 export default Contact;
 
-// {/* 
-//             <form onChange={(e) => handleSubmit(e)} name="contact" method="post"  data-netlify="true" data-netlify-honeypot="bot-field">
-//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "name")} value={name} label="name" type="name"/> 
-//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "email")} value={email} label="email" type="email"/> 
-//               <TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "message")} value={message} label="message" name="message" multiline rows={4}/>
-//               <Button type="submit" sx={{minWidth: "25%"}} variant="outlined">Submit</Button> 
-//             </form> */} */}
+{/* <form onChange={(e) => handleSubmit(e)} name="contact" method="post"  data-netlify="true" data-netlify-honeypot="bot-field">
+<TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "name")} value={name} label="name" type="name"/> 
+<TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "email")} value={email} label="email" type="email"/> 
+<TextField sx={{minWidth: "90%"}} onChange={(e) => handleChange(e, "message")} value={message} label="message" name="message" multiline rows={4}/>
+              <Button type="submit" sx={{minWidth: "25%"}} variant="outlined">Submit</Button>  */}
